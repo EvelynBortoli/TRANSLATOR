c011daf38c583704ab3182d0fdb0bc23dbc4c066
EvelynBortoli <76661672+EvelynBortoli@users.noreply.github.com>
2021-07-17 11:40:56 -0300

    Add files via upload

---
+++ TRANSLATOR/js/index.js
@@ -0,0 +1,5 @@
+const API_URL = 'api-free.deepl.com';
+const privateKey = "5652c0b9-adcf-7f2e-f6a2-3a577f700dc9:fx"
+TextToTranlate = document.getElementById('textToTranslate')
+
+translate = "Aqui estoy"
